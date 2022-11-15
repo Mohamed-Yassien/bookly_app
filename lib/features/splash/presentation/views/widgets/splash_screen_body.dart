7e5d88673538ddeb69b55946f3106a95aff2f7bc
Mohamed Yassien <moyassien219@gmail.com>
2022-11-15 16:12:49 +0200

    add sliding animation to splash screen

--- lib/features/splash/presentation/views/widgets/splash_screen_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_screen_body.dart
@@ -1,29 +1,77 @@
 import 'package:flutter/material.dart';
 
-class SplashScreenBody extends StatelessWidget {
+class SplashScreenBody extends StatefulWidget {
   const SplashScreenBody({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreenBody> createState() => _SplashScreenBodyState();
+}
+
+class _SplashScreenBodyState extends State<SplashScreenBody>
+    with TickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> subSlidingAnimation;
+  late Animation<Offset> baseSlidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    subSlidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 6), end: Offset.zero)
+            .animate(animationController);
+    baseSlidingAnimation =
+        Tween<Offset>(begin: const Offset(-6, 0), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: const [
-        Text(
-          'BoOkLY',
-          style: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.w900,
-          ),
-          textAlign: TextAlign.center,
+      children: [
+        AnimatedBuilder(
+          animation: baseSlidingAnimation,
+          builder: (context, _) {
+            return SlideTransition(
+              position: baseSlidingAnimation,
+              child: const Text(
+                'BoOkLY',
+                style: TextStyle(
+                  fontSize: 60,
+                  fontWeight: FontWeight.w900,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            );
+          },
         ),
-        Text(
-          'Read Free Books',
-          style: TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.w500,
-          ),
-          textAlign: TextAlign.center,
+        AnimatedBuilder(
+          animation: subSlidingAnimation,
+          builder: (context, _) {
+            return SlideTransition(
+              position: subSlidingAnimation,
+              child: const Text(
+                'Read Free Books',
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            );
+          },
         ),
       ],
     );
