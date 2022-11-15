55e39d0b4482776de4e377ead288c614a48df663
Mohamed Yassien <moyassien219@gmail.com>
2022-11-15 21:34:34 +0200

    add home screen custom app bar

---
+++ lib/features/splash/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 60.0),
+      child: Row(
+        children: [
+          const Text(
+            'BoOkLY',
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.w900,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const Spacer(),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              size: 25,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
